55fd31179a033d969972c7d6b51430953820ece3
Vishal-Malo <vishalmalo272000@gmail.com>
2020-06-18 21:36:32 +0530

    Web Page requirements

--- application.py
+++ application.py
@@ -4,8 +4,7 @@ from sklearn.linear_model import LinearRegression
 import numpy as np
 import pandas as pd
 
-application = app = Flask(__name__)
-
+app = Flask(__name__)
 
 @app.route('/')
 def index():
